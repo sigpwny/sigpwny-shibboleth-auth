52978c4ab037bca2f5a4e7556736181ebbba56d6
Ankur Sundara <Ankur.Sundara@leviathansecurity.com>
2020-08-03 18:43:44 -0400

    move config to env variables for better docker support

---
+++ app/src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	"DISCORD_CLIENT_ID": "" || process.env.DISCORD_CLIENT_ID,
+	"DISCORD_CLIENT_SECRET": "" || process.env.DISCORD_CLIENT_SECRET,
+    "DISCORD_BOT_TOKEN": "" || process.env.DISCORD_BOT_TOKEN
+}
