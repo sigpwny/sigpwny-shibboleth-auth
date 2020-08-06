0a0ae749f5f5207f3fdf165c314b8ad759de250a
Ankur Sundara <me@ankursundara.com>
2020-08-06 14:09:21 -0400

    2.0 first commit

---
+++ app/src/config.ts
@@ -0,0 +1,6 @@
+export default {
+    DISCORD_CLIENT_ID: "" || process.env.DISCORD_CLIENT_ID,
+	DISCORD_CLIENT_SECRET: "" || process.env.DISCORD_CLIENT_SECRET,
+    DISCORD_BOT_TOKEN: "" || process.env.DISCORD_BOT_TOKEN,
+    APP_HOSTNAME: "" || process.env.APP_HOSTNAME
+}
---
+++ app/src/server.ts
@@ -0,0 +1,63 @@
+import Koa from 'koa';
+import Router from 'koa-joi-router';
+import bodyParser from 'koa-bodyparser';
+import ClientOAuth2 from 'client-oauth2';
+import Keyv from 'keyv';
+import { v4 as uuidv4, v1 as uuidv1 } from 'uuid';
+
+import config from './config';
+
+const app = new Koa();
+// middleware to parse form bodies
+app.use(bodyParser());
+const Joi = Router.Joi;
+const router = Router();
+
+const SESSION_TTL_SECONDS = 600
+// TODO - redis backend for keyv
+const sessionStore = new Keyv(undefined, { ttl: SESSION_TTL_SECONDS*1000 });
+sessionStore.on('error', err => console.log('Connection Error', err));
+
+const discordAuth = new ClientOAuth2({
+    clientId: config.DISCORD_CLIENT_ID,
+    clientSecret: config.DISCORD_CLIENT_SECRET,
+    accessTokenUri: 'https://discordapp.com/api/oauth2/token',
+    authorizationUri: 'https://discordapp.com/api/oauth2/authorize',
+    redirectUri: `${config.APP_HOSTNAME}/auth/discord/callback`
+  });
+
+router.get('/api/login/:id', {
+    validate: {
+        params: {
+            id: Joi.string()
+        }
+    }
+}, async (ctx) => {
+    ctx.body = ctx.request.params.id;
+});
+
+router.get('/auth/discord', async (ctx) => {
+
+    ctx.redirect(discordAuth.code.getUri());
+});
+
+router.get('/auth/discord/callback', {
+    validate: {
+        query: {
+            code: Joi.string(),
+            state: Joi.string()
+        }
+    }
+}, async (ctx) => {
+    const stateUid = uuidv4();
+    const userToken = await discordAuth.code.getToken(ctx.url, {state: stateUid});
+    ctx.body = "todo"
+});
+router.get('/', async (ctx) => {
+    ctx.body = 'Hello World!';
+});
+app.use(router.middleware());
+
+app.listen(8080);
+
+console.log('Server running on port 8080');
